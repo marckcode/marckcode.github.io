dd0c5754cbef6be2069c70728c1a3c82ddf51a4b
marckcode <marckcode@gmail.com>
2024-02-29 12:54:05 -0300

    Add button func md

--- assets/js/copybutton.js
+++ assets/js/copybutton.js
@@ -1,16 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let codeBlocks = document.querySelectorAll("pre");
-    
-    codeBlocks.forEach(function(block) {
-        let copyButton = document.createElement("button");
-        copyButton.className = "button-copy-code";
-        copyButton.textContent = "Copy";
-        
-        copyButton.addEventListener("click", async function() {
-            await navigator.clipboard.writeText(block.textContent);
-            alert("Code copied to clipboard!");
-        });
-        
-        block.appendChild(copyButton);
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    if (!navigator.clipboard) {
+        return;
+    }
+
+    let button = document.createElement("button");
+    button.className = "button-copy-code";
+    button.innerHTML = copyIcon;
+    block.appendChild(button);
+
+    button.addEventListener("click", async () => {
+        await copyCode(block);
     });
 });
+
+async function copyCode(block) {
+    let copiedCode = block.cloneNode(true);
+    copiedCode.removeChild(copiedCode.querySelector("button.button-copy-code"));
+
+    const html = copiedCode.outerHTML.replace(/<[^>]*>?/gm, "");
+
+    block.querySelector("button.button-copy-code").innerHTML = copiedIcon;
+    setTimeout(function () {
+        block.querySelector("button.button-copy-code").innerHTML = copyIcon;
+    }, 2000);
+
+    const parsedHTML = htmlDecode(html);
+
+    await navigator.clipboard.writeText(parsedHTML);
+}
+
+function htmlDecode(input) {
+    const doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+}
